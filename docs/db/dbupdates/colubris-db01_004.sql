9c829be7519dc72fc604ac7eaffcbd4dac818057
Romans Malinovskis <r@agiletech.ie>
2011-02-04 19:02:10 +0000

    Minor changes
    Switching to devel branch of atk4 and addons
    If 4th update fails due to missing filestore, it can be re-run successfully

--- docs/db/dbupdates/colubris-db01_004.sql
+++ docs/db/dbupdates/colubris-db01_004.sql
@@ -2,14 +2,6 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-ALTER TABLE `screen` ADD COLUMN `filestore_file_id` INT(11) NOT NULL  AFTER `budget_id` , 
-  ADD CONSTRAINT `fk_screen_filestore_file1`
-  FOREIGN KEY (`filestore_file_id` )
-  REFERENCES `filestore_file` (`id` )
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION
-, ADD INDEX `fk_screen_filestore_file1` (`filestore_file_id` ASC) ;
-
 ALTER TABLE `filestore_file` 
   ADD CONSTRAINT `fk_filestore_file_filestore_type1`
   FOREIGN KEY (`filestore_type_id` )
@@ -23,6 +15,15 @@ ALTER TABLE `filestore_file`
   ON UPDATE NO ACTION
 , ADD INDEX `fk_filestore_file_filestore_type1` (`filestore_type_id` ASC) 
 , ADD INDEX `fk_filestore_file_filestore_volume1` (`filestore_volume_id` ASC) ;
+
+ALTER TABLE `screen` ADD COLUMN `filestore_file_id` INT(11) NOT NULL  AFTER `budget_id` , 
+  ADD CONSTRAINT `fk_screen_filestore_file1`
+  FOREIGN KEY (`filestore_file_id` )
+  REFERENCES `filestore_file` (`id` )
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+, ADD INDEX `fk_screen_filestore_file1` (`filestore_file_id` ASC) ;
+
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
