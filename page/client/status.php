53e7a53612fd5a46ebd58acfd314da3cdb4bfe15
prashanth pratapagiri <apple.php>
2011-06-09 19:27:17 +0530

    client sample pages

--- page/client/status.php
+++ page/client/status.php
@@ -3,13 +3,14 @@ class page_client_status extends Page_Client {
 	function init(){
 		parent::init();
 
+                $this->add('H1')->set('Project Status');
 
-		$this->add('Button')->setLabel('Request Screen')
-			->js('click')->univ()->dialogURL('New Screen Request',
-					$this->api->getDestinationURL('client/screenrequest'));
-
-		$l=$this->add('View_Screens');
-		$this->limitTourrentProject($l);
+//		$this->add('Button')->setLabel('Request Screen')
+//			->js('click')->univ()->dialogURL('New Screen Request',
+//					$this->api->getDestinationURL('client/screenrequest'));
+//
+//		$l=$this->add('View_Screens');
+//		$this->limitTourrentProject($l);
 
 
 	}
---
+++ page/client/timesheets.php
@@ -0,0 +1,11 @@
+<?
+class page_client_timesheets extends Page_Client {
+	function init(){
+		parent::init();
+
+                $this->add('H1')->set('Time Sheets');
+
+
+	}
+
+}
