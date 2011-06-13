7dd4dc2e2686ca162b9bbaa064366661e79d72a4
Romans Malinovskis <r@agiletech.ie>
2011-06-13 02:41:49 +0100

    fix budget filtering

--- page/team/budgets.php
+++ page/team/budgets.php
@@ -5,24 +5,13 @@ class page_team_budgets extends Page {
     //public $controller='Controller_Budget';
 
     function initMainPage() {
-        $model = $this->add('Model_Budget');
-        $model->addCondition('closed', 'N');
+        $model = $this->add('Model_Payment');
+        $model->addCondition('user_id',$this->api->getUserID());
 
         $c = $this->add('CRUD_TeamBudgets');
-        $model->getField('days_spent')->caption('Cur Mandays');
-        $c->setModel($model, array('name', "deadline", 'accepted', 'mandays', 'days_spent', 'project_id'));
+        //$model->getField('days_spent')->caption('Cur Mandays');
+        $c->setModel($model, array('budget'));
         if ($c->grid) {
-
-           $g=$c->grid;
-           $c->grid->last_column = 'deadline';
-            $c->grid->makeSortable();
-             $c->grid->last_column = 'accepted';
-            $c->grid->makeSortable();
-             $c->grid->last_column = 'mandays';
-            $c->grid->makeSortable();
-           //Depleted % = Cur Mandays / Mandays (Precentage). Red if its >100%
-           //refer to http://agiletoolkit.org/doc/grid/columns
-            $g->addColumn('html', 'depleted', 'Depleted %');
         }
     }
 
